c7c7967ad088d23a8d061116adbaf9d65930ea65
Pranav Kalambe <pranavkalambe12@gmail.com>
2023-04-24 10:59:56 +0530

    multi PC network setup

--- ethereum/constants.js
+++ ethereum/constants.js
@@ -1,16 +1,16 @@
 const Web3 = require('web3')
 
 
-const smartContractAddress = "0x15a7fb3214f3E5e49e4Cd3b600120FBC73050C2E";
-const smartContractAddressGlobal = "0x17808Baf94cB48ac2F1BE866360C098b9d7B5a63";
+const smartContractAddress = "0xB5DD0E66d22915288CF6A6A560fE4EC5345BbD57";
+const smartContractAddressGlobal = "0x183E527dD34a8f9f1f4CA7f52F4CE63024e705f0";
 
-const sensorNodeAddr ="0x0599acace78742e703dead19ab3dec9e6707c209";
-const farmManagerAddr ="0xa5eb9b74be8ce1d5acebc86a0305aa5c7123a322";
-const farmManagerAddrGlobal ="0x7451a3d0310dc99f3eff4b85e6dc78a20550edd4";
+const sensorNodeAddr ="0x6df2c4db72a6bfffd05b573d9a676460574fbd45";
+const farmManagerAddr ="0x1eb8dca855677c48ac6352dd3bb2aaab0ffe8fa3";
+const farmManagerAddrGlobal ="0x259b9dae53619179fbe4aa399812c8ecdcb5cf23";
 
 const httpAddr_manager = "http://127.0.0.1:8041";
 const httpAddr_sensor = "http://127.0.0.1:8042"; 
-const httpAddr_globalManager = "http://127.0.0.1:9041";      
+const httpAddr_globalManager = "http://172.17.22.51:9041";      
 
 //sensor node address
 const web3_sensor = new Web3(new Web3.providers.HttpProvider(httpAddr_sensor));
